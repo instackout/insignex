cc351b0995c725793a5942fc39f1ffa168ab81b3
instackout <instackout@gmail.com>
2023-04-07 15:23:13 +0530

    Loader issue resolved

--- js/common/common.js
+++ js/common/common.js
@@ -8,17 +8,17 @@ var spinner = function () {
 };
 
 $(function () {
-    $('head').load('common/head.html');
-    $('#spinner').load('common/spinner.html');
-    $('#header').load('common/header.html', function () {
-        let url = window.location.pathname;
-        url = url.substring(url.lastIndexOf('/') + 1);
-
-        $('.nav-link').removeClass('active');
-        $('.nav-link[href="' + url + '"]').addClass('active');
-    });
-    $('#footer').load('common/footer.html', function () {
-        $('#currYear').html(new Date().getFullYear());
-        spinner();
+    $("#spinner").load("common/spinner.html", function () {
+        $("#header").load("common/header.html", function () {
+            let url = window.location.pathname;
+            url = url.substring(url.lastIndexOf("/") + 1);
+        
+            $(".nav-link").removeClass("active");        
+            $('.nav-link[href="' + url + '"]').addClass("active");
+        });
+        $('#footer').load('common/footer.html', function () {
+            $('#currYear').html(new Date().getFullYear());
+            spinner();
+        });
     });
 });
