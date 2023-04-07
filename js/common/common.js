9b200baed751b5beeba06f1324d9f914a7173f5e
instackout <instackout@gmail.com>
2023-04-07 10:06:46 +0530

    Logging

--- js/common/common.js
+++ js/common/common.js
@@ -2,6 +2,7 @@ $(function () {
     $('head').load('common/head.html');
     $('#header').load('common/header.html', function () {
         let url = window.location.pathname;
+        console.log(url);
         url = url == '/' ? '/' : url.split('/').join('');
 
         $('.nav-link').removeClass('active');
